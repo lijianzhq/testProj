b35785e999dd50f3cfb772087625e61f2c5a0293
jianl <804390150@qq.com>
2020-08-27 20:39:13 +0800

    签入测试代码。

---
+++ TestProj/TestCoreConsole/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace TestCoreConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("start test");
+
+            //测试IsAssignableFrom
+            TestIsAssignableFrom.Start();
+
+            Console.WriteLine("end test");
+            Console.ReadLine();
+        }
+    }
+}
